ebe507318375db01061003fa1414cebac8cc4409
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2024-06-19 23:04:59 +0530

    Update Contact.tsx

--- app/contact-section/Contact.tsx
+++ app/contact-section/Contact.tsx
@@ -27,7 +27,7 @@ const Contact = () => {
             }
           />
           <Image
-            src={Heart Icon}
+            src={heart Icon}
             alt="Heart Icon"
             className="heartbeat md:-bottom-18 absolute -bottom-5 left-64 w-[120px] sm:-bottom-14 sm:left-[40%] md:left-[40%] md:w-[150px] lg:-bottom-16 lg:left-[42%] lg:w-[230px]"
           />
