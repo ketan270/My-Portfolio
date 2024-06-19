868b340041854d67ec416990a4c8f7e1825bf282
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2024-06-20 00:05:34 +0530

    Update Contact.tsx

--- app/contact-section/Contact.tsx
+++ app/contact-section/Contact.tsx
@@ -26,11 +26,11 @@ const Contact = () => {
               "flex max-w-[500px] flex-col items-start text-left text-[150px] font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center sm:text-[170px] md:text-[200px] lg:text-center lg:text-[270px] xl:text-[390px]"
             }
           />
-          <Image
+{/*           <Image
             src={heartIcon}
             alt="HeartIcon"
             className="heartbeat md:-bottom-18 absolute -bottom-5 left-64 w-[120px] sm:-bottom-14 sm:left-[40%] md:left-[40%] md:w-[150px] lg:-bottom-16 lg:left-[42%] lg:w-[230px]"
-          />
+          /> */}
         </div>
 
         <div className="mt-20 flex w-full flex-col items-end justify-center gap-16 sm:mt-32 sm:gap-12 md:mt-40 md:flex-row md:items-start md:justify-between lg:mt-12 lg:max-w-[1440px]">
